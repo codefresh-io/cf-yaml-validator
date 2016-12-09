fd3c79afe589652be1cec299450ae16f06668e32
Itai Gendler <itai@codefresh.io>
2016-12-09 02:18:59 +0200

    Add support of ecr step push fields

--- schema/1.0/steps/push.js
+++ schema/1.0/steps/push.js
@@ -23,11 +23,16 @@ class Push extends BaseSchema {
 
     getSchema() {
         let pushProperties = {
-            type:        Joi.string().valid(Push.getType()),
-            candidate:   Joi.string().required(),
-            registry:    Joi.string(),
-            credentials: BaseSchema._getCredentialsSchema(),
-            tag:         Joi.string()
+            type:            Joi.string().valid(Push.getType()),
+            provider:        Joi.string().regex(/^docker|ecr$/),
+            candidate:       Joi.string().required(),
+            registry:        Joi.string(),
+            credentials:     BaseSchema._getCredentialsSchema(),
+            tag:             Joi.string(),
+            accessKeyId:     Joi.string(),
+            secretAccessKey: Joi.string(),
+            region:          Joi.string()
+
         };
         return this._createSchema(pushProperties);
     }
