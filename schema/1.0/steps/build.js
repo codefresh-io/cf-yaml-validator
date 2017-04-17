4ab28fcab2cf6f69a9ef9c3ace105326f5d96f9c
Itai Gendler <itai@codefresh.io>
2017-04-17 16:21:41 -0400

    replace build's image name with repository

--- schema/1.0/steps/build.js
+++ schema/1.0/steps/build.js
@@ -29,7 +29,7 @@ class Build extends BaseSchema {
                                    .try(Joi.string(), Joi.object({ content: Joi.string() })),
             no_cache:          Joi.boolean(),
             squash:            Joi.boolean(),
-            image_name:        Joi.string().required(),
+            repository:        Joi.string().required(),
             build_arguments:   Joi.array().items(Joi.string()),
             tag:               Joi.string(),
             metadata:          Joi.object({
