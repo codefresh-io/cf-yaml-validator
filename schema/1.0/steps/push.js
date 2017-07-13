b8be59c0baaf74bdcd10030990cdca5c4e39a342
Alon Diamant <alon@codefresh.io>
2017-07-13 22:58:45 +0300

    Fixed to push tags

--- schema/1.0/steps/push.js
+++ schema/1.0/steps/push.js
@@ -22,19 +22,6 @@ class Push extends BaseSchema {
     }
 
     getSchema() {
-        let pushTagProperties = {
-            type:            Joi.string().valid(Push.getType()),
-            provider:        Joi.string().regex(/^standard|docker|ecr$/),
-            candidate:       Joi.string().required(),
-            registry:        Joi.string(),
-            credentials:     BaseSchema._getCredentialsSchema(),
-            tag:             Joi.string(),
-            image_name:      Joi.string(),
-            accessKeyId:     Joi.string(),
-            secretAccessKey: Joi.string(),
-            region:          Joi.string()
-
-        };
 
         let pushTagsProperties = {
             type:            Joi.string().valid(Push.getType()),
@@ -42,6 +29,7 @@ class Push extends BaseSchema {
             candidate:       Joi.string().required(),
             registry:        Joi.string(),
             credentials:     BaseSchema._getCredentialsSchema(),
+            tag:             Joi.string(),
             tags:            Joi.array().items(Joi.string()),
             image_name:      Joi.string(),
             accessKeyId:     Joi.string(),
@@ -50,13 +38,7 @@ class Push extends BaseSchema {
 
         };
 
-
-        const pushProperties = Joi.alternatives().try(
-            pushTagProperties,
-            pushTagsProperties
-        );
-
-        return this._createSchema(pushProperties);
+        return this._createSchema(pushTagsProperties);
     }
 }
 // Exported objects/methods
