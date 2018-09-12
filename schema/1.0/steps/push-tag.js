80e594a37e69f910276f2e4137d118e6498f08af
pasha <pavel@codefresh.io>
2018-09-12 18:54:03 +0300

    add push tag validation

---
+++ schema/1.0/steps/push-tag.js
@@ -0,0 +1,38 @@
+/**
+ * Defines the push step schema
+ */
+
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const Joi        = require('joi');
+const BaseSchema = require('./../base-schema');
+
+class PushTag extends BaseSchema {
+
+    //------------------------------------------------------------------------------
+    // Public Interface
+    //------------------------------------------------------------------------------
+
+    static getType() {
+        return 'push-tag';
+    }
+
+    getSchema() {
+
+        let pushTagsProperties = {
+            title:           Joi.string(),
+            type:            Joi.string().valid(PushTag.getType()),
+            image_name:      Joi.string().required(),
+            tags:            Joi.array().items(Joi.string()).required(),
+
+        };
+
+        return this._createSchema(pushTagsProperties);
+    }
+}
+// Exported objects/methods
+module.exports = PushTag;
