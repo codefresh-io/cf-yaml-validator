16ed5a51e4e66ceafe20185e8def428547c8ea2b
andrii.shaforostov <andrii@codefresh.io>
2019-07-26 16:35:13 +0300

    debug before and after

--- schema/1.0/steps/freestyle.js
+++ schema/1.0/steps/freestyle.js
@@ -31,7 +31,8 @@ class Freestyle extends BaseSchema {
             environment: Joi.array().items(Joi.string()),
             entry_point: Joi.alternatives().try(Joi.string(), Joi.array().items(Joi.string())),
             shell: Joi.string().valid('sh', 'bash'),
-            debug: Joi.boolean()
+            debugBefore: Joi.boolean(),
+            debugAfter: Joi.boolean()
         };
         return this._createSchema(freestyleProperties)
             .without('commands', 'cmd') // make sure cmd and commands are mutually exclusive AND optional
