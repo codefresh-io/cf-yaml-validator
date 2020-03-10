65aa73132c980d12ad3e2c1ebbe2e169c37b48d9
Vadim Kharin <vadim.kharin@codefresh.io>
2020-03-10 15:17:00 +0200

    SAAS-6139 - fix ip url validation

--- schema/1.0/validations/registry.js
+++ schema/1.0/validations/registry.js
@@ -7,8 +7,14 @@ const { docBaseUrl, DocumentationLinks, IntegrationLinks } = require('./../docum
 
 const isWebUri = function (s) {
     if (s) {
-        const res = s.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/g);
-        return (res !== null);
+        const pattern = new RegExp('^((ft|htt)ps?:\\/\\/)?' // protocol
+            + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name and extension
+            + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
+            + '(\\:\\d+)?' // port
+            + '(\\/[-a-z\\d%@_.~+&:]*)*' // path
+            + '(\\?[;&a-z\\d%@_.,~+&:=-]*)?' // query string
+            + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return !!pattern.test(s);
     }
     return false;
 };
