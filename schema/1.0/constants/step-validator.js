8ab34118e87cfd917700808d67e75a3c280da435
Vadim Kharin <vadim.kharin@codefresh.io>
2021-02-10 16:01:22 +0200

    CR-3228 Add warning to yaml-validator about helm 2 deprecation

--- schema/1.0/constants/step-validator.js
+++ schema/1.0/constants/step-validator.js
@@ -6,6 +6,7 @@ const Push = require('../steps/push');
 const Build = require('../steps/build');
 const Freestyle = require('../steps/freestyle');
 const Composition = require('../steps/composition');
+const Helm = require('../steps/helm');
 
 const StepValidator = {
     'git-clone': GitClone,
@@ -13,6 +14,7 @@ const StepValidator = {
     'push': Push,
     'build': Build,
     'freestyle': Freestyle,
+    'helm': Helm,
     'composition': Composition
 };
 
---
+++ schema/1.0/steps/helm.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const _ = require('lodash');
+const Joi        = require('joi');
+const BaseSchema = require('./../base-schema');
+const { ErrorType, ErrorBuilder } = require('./../error-builder');
+const { IntegrationLinks } = require('./../documentation-links');
+
+class Helm extends BaseSchema {
+
+    static getType() {
+        return 'helm';
+    }
+
+    getSchema() {
+        const HelmProperties = {
+            'type': Joi.string().valid(Helm.getType()),
+            'working_directory': Joi.string(),
+        };
+        return this._createSchema(HelmProperties)
+            .unknown();
+    }
+
+    _applyStepCompatibility(schema) {
+        return schema.rename('working-directory', 'working_directory', { ignoreUndefined: true });
+    }
+
+    static validateStep(step, yaml, name, context, { ignoreValidation }) {
+        const warnings = [];
+
+        const helmVersion = _.get(step, 'arguments.helm_version', '3');
+        if (!ignoreValidation && helmVersion.startsWith('2')) {
+            warnings.push(ErrorBuilder.buildError({
+                message: `Helm 2 support will be deprecated soon`,
+                name,
+                yaml,
+                code: 601,
+                type: ErrorType.Warning,
+                docsLink: _.get(IntegrationLinks, 'helm'),
+                errorPath: 'helm',
+                key: 'helm_version',
+            }));
+        }
+
+        return { errors: [], warnings };
+    }
+}
+
+
+module.exports = Helm;
