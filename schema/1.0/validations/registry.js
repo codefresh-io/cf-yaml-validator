6f044822dba3671b2c64bbd48e11cf610e389613
elad-codefresh <elad.laor@codefresh.io>
2022-03-08 16:58:29 +0200

    for freestyle only

--- schema/1.0/validations/registry.js
+++ schema/1.0/validations/registry.js
@@ -112,7 +112,7 @@ const validateRegistryContext = function (step,
         }));
     }
 
-    if (!registryContext && step.role_arn) {
+    if (stepType === 'freestyle' && !registryContext && step.role_arn) {
         errors.push(ErrorBuilder.buildError({
             message: `Cross-account pulling requires specifying a registry integration`,
             name,
